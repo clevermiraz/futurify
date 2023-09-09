a8e1eab48127b4a0f6d605aa3dd35dec22d06da2
clevermiraz <miraz.onesoft@gmail.com>
2023-09-10 00:11:56 +0600

    Code Generation AI (Open AI) complete

---
+++ app/(dashboard)/(routes)/code/page.tsx
@@ -0,0 +1,152 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
+import { Code } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { OpenAI } from "openai";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import ReactMarkdown from "react-markdown";
+import * as z from "zod";
+
+import { BotAvatar } from "@/components/bot-avatar";
+import Empty from "@/components/empty";
+import Heading from "@/components/heading";
+import Loader from "@/components/loader";
+import { Button } from "@/components/ui/button";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { UserAvatar } from "@/components/user-avatar";
+import { cn } from "@/lib/utils";
+import { formSchema } from "./constants";
+
+export default function CodePage() {
+    const router = useRouter();
+    const [messages, setMessages] = useState<
+        OpenAI.Chat.CreateChatCompletionRequestMessage[]
+    >([]);
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            prompt: "",
+        },
+    });
+
+    const isLoading = form.formState.isSubmitting;
+
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        try {
+            const userMessage: OpenAI.Chat.CreateChatCompletionRequestMessage =
+                {
+                    role: "user",
+                    content: values.prompt,
+                };
+            const newMessages = [...messages, userMessage];
+
+            const response = await axios.post("/api/code", {
+                messages: newMessages,
+            });
+
+            setMessages((current) => [...current, response.data, userMessage]);
+
+            form.reset();
+        } catch (error: any) {
+            console.log(error);
+        } finally {
+            router.refresh();
+        }
+    };
+
+    return (
+        <>
+            <Heading
+                title="Code Generation"
+                description="Generate code from natural language"
+                icon={Code}
+                iconColor="text-green-700"
+                bgColor="bg-green-500/10"
+            />
+            <div className="px-4 lg:px-8">
+                <div>
+                    <Form {...form}>
+                        <form
+                            action=""
+                            onSubmit={form.handleSubmit(onSubmit)}
+                            className="rounded-lg border w-full p-4 px-3 md:px-6 focus-within:shadow grid grid-cols-12 gap-2"
+                        >
+                            <FormField
+                                name="prompt"
+                                render={({ field }) => (
+                                    <FormItem className="col-span-12 lg:col-span-10">
+                                        <FormControl className="m-0 p-0">
+                                            <Input
+                                                className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transpare"
+                                                disabled={isLoading}
+                                                placeholder="Give me a Python function that returns the sum of two numbers"
+                                                {...field}
+                                            />
+                                        </FormControl>
+                                    </FormItem>
+                                )}
+                            />
+                            <Button
+                                className="col-span-12 lg:col-span-2 w-full"
+                                disabled={isLoading}
+                            >
+                                Generate
+                            </Button>
+                        </form>
+                    </Form>
+                </div>
+                <div className="space-y-4 mt-4">
+                    {isLoading && <Loader />}
+
+                    {messages.length === 0 && !isLoading && (
+                        <Empty label="No Messages Yet" />
+                    )}
+                    <div className="flex flex-col-reverse gap-y-4">
+                        {messages.map((message, index) => (
+                            <div
+                                key={index}
+                                className={cn(
+                                    "p-8 w-full flex items-start gap-x-8 rounded-lg",
+                                    message.role === "user"
+                                        ? "bg-white border border-black/10"
+                                        : "bg-muted"
+                                )}
+                            >
+                                {message.role === "user" ? (
+                                    <UserAvatar />
+                                ) : (
+                                    <BotAvatar />
+                                )}
+                                <div className="text-sm">
+                                    <ReactMarkdown
+                                        components={{
+                                            pre: ({ node, ...props }) => (
+                                                <div className="overflow-auto w-full my-2 bg-black/10 p-2 rounded-lg">
+                                                    <pre {...props} />
+                                                </div>
+                                            ),
+                                            code: ({ node, ...props }) => (
+                                                <code
+                                                    className="bg-black/10 rounded-lg p-1"
+                                                    {...props}
+                                                />
+                                            ),
+                                        }}
+                                        className="text-sm overflow-hidden leading-7"
+                                    >
+                                        {message.content || ""}
+                                    </ReactMarkdown>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
