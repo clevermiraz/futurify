d5e88c6a78d3a4c20b79f98deab24cfefe3648d0
clevermiraz <miraz.onesoft@gmail.com>
2023-09-30 13:11:18 +0600

    Modify next.config and Ignore type checking

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
     images: {
         domains: ["oaidalleapiprodscus.blob.core.windows.net"],
     },
+    typescript: {
+        ignoreBuildErrors: true,
+    },
 };
 
 module.exports = nextConfig;
